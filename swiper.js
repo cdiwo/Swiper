254f13ed2bec2d9f3497110810cefec22cd441e6
David Wei <cdiwo@qq.com>
2016-05-31 17:36:04 +0800

    新增垂直模式

--- swiper.js
+++ swiper.js
@@ -1,10 +1,10 @@
 /**
- * Swiper v1.0.1
+ * Swiper v1.1.
  * @create Mar 17, 2015
  * @author David Wei
  * @see http://github.com/cdiwo/Swiper
  * Licensed under MIT
- * Released on: Mar 23, 2015
+ * Released on: May 31, 2016
  */
 (function () {
     'use strict';
@@ -13,13 +13,15 @@
     Swiper
     ===========================*/
     function Swiper(container, params) {
-        
+        // Swiper
+        var s = this;
+
         var defaults = {
             direction: 'horizental', // 方向
             autoplay: false,// autoplay
             speed: 300,// Css3 animation duration
             easing: 'ease',// Css3 animation timing function
-            inertia: true,// 开启惯性滑动
+            inertia: false,// 开启惯性滑动
             touchRatio: 0.2,// 拖拽占比率，默认元素的1/5
             index: 0,// 初始下标
             wrapperClass: '.swiper-wrapper',
@@ -43,24 +45,14 @@
         for (var param in params) {
             defaults[param] = params[param];
         }
-
-        // Swiper
-        var s = this;
-
-        // Params
         s.params = defaults;
 
-        // Define Container, Wrapper and Slide
+        // 滑动层参数
+        var wrapper, wrap = {}, slide;
+
+        // Define Container, Wrapper
         if(container === undefined) throw new Error('Invalid container!');
-        s.container =  container === window ? $(window) : $(container);
-        s.wrapper = s.container.find(s.params.wrapperClass);
-        s.slide = s.container.find(s.params.slideClass);
-
-        // Is horizental
-        function isH() {
-            return s.params.direction === 'horizental';
-        }
-
+        
         // Touch Vars
         s.touchObject = {};//拖拽对象
         s.animType, s.transitionType, s.transformType, s.transformsEnabled;//Css3 Transition 相关参数
@@ -71,15 +63,14 @@
         s.dragging = false;//正在拖拽
         s.edgeFriction = 0.35;//边缘减速系数
         s.inertiaFriction = 0.01;//惯性滑动系数
-        s.position = 0;
-        s.lastPosition = 0;//定位
-        s.index = 0;//最后一次的下标
-
-        // Slide Vars
-        s.slideWidth, s.slideCount;//元素宽度px和个数
 
         // Autoplay paused
         s.autoplayPaused = false;
+
+        // Is horizental
+        function isH() {
+            return s.params.direction === 'horizental';
+        }
 
         /*=========================
         Slide progress
@@ -87,47 +78,46 @@
         s.getIndex = function() {
             // var verticalHeight;
             // verticalHeight = s.slide.outerHeight();
-            if (s.position > 0) {
+            if (wrap.position > 0) {
                 return 0;
             }
-            var index = Math.floor(Math.abs(s.position) / s.slideWidth);
+            var index = Math.floor(Math.abs(wrap.position) / wrap.slideWH);
             // 快速滑动
             if((s.touchObject.endTime - s.touchObject.startTime < s.touchDelay)) {
-                if(s.swipeDirection() === "left")
+                if(s.swipeDirection() === "left" || s.swipeDirection() === "up")
                     index += 1;
             } 
             // 移动超过1/2，下标向后移动
-            else if(Math.abs(s.position) % s.slideWidth / s.slideWidth > 1 / 2) {
+            else if(Math.abs(wrap.position) % wrap.slideWH / wrap.slideWH > 1 / 2) {
                 index += 1;
             }
-            return (index >= s.slideCount) ? (s.slideCount - 1) : index;
+            return (index >= wrap.slideCount) ? (wrap.slideCount - 1) : index;
         };
-        s.swipeDirection = function(){
+        s.swipeDirection = function() {
             if(isH())
                 return s.touchObject.startX - s.touchObject.curX > 0 ? 'left' : 'right';
             else
                 return s.touchObject.startY - s.touchObject.curY > 0 ? 'down' : 'up';
         };
-        
 
         /*=========================
         Transitions
         ===========================*/
         s.slideTo = function(index) {
-            var indexChanged = s.index != index,
-            targetLeft = index * s.slideWidth;
-            targetLeft = -targetLeft;
+            var indexChanged = wrap.index != index,
+            targetPosition = index * wrap.slideWH;
+            targetPosition = -targetPosition;
 
             // 记住最后的定位和下标
-            s.lastPosition = targetLeft;
-            s.index = index;
+            wrap.lastPosition = targetPosition;
+            wrap.index = index;
 
             // apply transform
-            s.applyTransform(targetLeft);
+            s.applyTransform(targetPosition);
 
             // disable transition
             setTimeout(function(){
-                s.wrapper.css(s.transitionType, '');
+                wrapper.css(s.transitionType, '');
                 s.trigger('onSlideEnd', index);
             }, s.params.speed);
 
@@ -139,23 +129,25 @@
             s.touchObject = {};
         };
         s.next = function() {
-            var index = s.index >= s.slideCount - 1 ? 0 : s.index + 1;
+            var index = wrap.index >= wrap.slideCount - 1 ? 0 : wrap.index + 1;
             s.slideTo(index);
         };
         s.prev = function() {
-            var index = s.index <= 0 ? s.slideCount - 1 : s.index - 1;
+            var index = wrap.index <= 0 ? wrap.slideCount - 1 : wrap.index - 1;
             s.slideTo(index);
         };
 
         /*=========================
         Translate/transition helpers
         ===========================*/
-        s.applyTransform = function(x, y, speed) {
-            var x = x || 0, y = y || 0, speed = speed || s.params.speed;
+        s.applyTransform = function(len, speed, type) {
+            var len = len || 0, speed = speed || s.params.speed;
+            var xy = isH() ?  len + 'px, 0px' : '0px, ' +  len + 'px';
             // apply transation
-            s.wrapper.css(s.transitionType, s.transformType + ' ' + speed + 'ms ' + s.params.easing);
+            if(type !== false) 
+                wrapper.css(s.transitionType, s.transformType + ' ' + speed + 'ms ' + s.params.easing);
             // apply animation
-            s.wrapper.css(s.animType, 'translate(' + x + 'px, ' + y + 'px)');
+            wrapper.css(s.animType, 'translate(' + xy + ')');
         };
         s.setProps = function() {
             var bodyStyle = document.body.style;
@@ -177,35 +169,12 @@
         /*=========================
         Custom Events/Callbacks Trigger
         ===========================*/
-        s.eventListeners = {};
         s.trigger = function (eventName) {
             // Trigger callbacks
             if (s.params[eventName]) {
                 s.params[eventName](arguments[1], arguments[2], arguments[3]);
             }
-            
-            // Trigger events
-            var i, len;
-            if (s.eventListeners[eventName]) {
-                for (i = 0, len = s.eventListeners[eventName].length; i < len; i++) {
-                    s.eventListeners[eventName][i](arguments[1], arguments[2], arguments[3]);
-                }
-            }
         };
-        s.on = function (eventName, handler) {
-            function normalizeEventName (eventName) {
-                if (eventName.indexOf('on') !== 0) {
-                    eventName = 'on' + eventName[0].toUpperCase() + eventName.substring(1);
-                }
-                return eventName;
-            }
-            eventName = normalizeEventName(eventName);
-            if (!s.eventListeners[eventName]) 
-                s.eventListeners[eventName] = [];
-            s.eventListeners[eventName].push(handler);
-            return s;
-        };
-
         /*=========================
         Events Handle
         ===========================*/
@@ -230,18 +199,21 @@
             var touches = event.originalEvent !== undefined ? event.originalEvent.touches : null;
 
             // 触发了touchstart、mousedown，且有拖拽点
-            if (!s.dragging || touches && touches.length !== 1) {
-                return false;
-            }
+            if (!s.dragging || touches && touches.length !== 1) return false;
 
             // 设置当前坐标值
             s.touchObject.curX = touches !== undefined ? touches[0].pageX : event.clientX;
             s.touchObject.curY = touches !== undefined ? touches[0].pageY : event.clientY;
 
+            // 移动到顶部后，手动触发touchend事件
+            if(s.touchObject.curY  <= 0) return s.swipeEnd(event);
+
             // 计算滑动距离
             swipeLength = s.touchObject.swipeLength = Math.round(
                 Math.sqrt(
-                    Math.pow(s.touchObject.curX - s.touchObject.startX, 2)
+                    Math.pow(
+                        isH() ? (s.touchObject.curX - s.touchObject.startX) : (s.touchObject.curY - s.touchObject.startY)
+                    , 2)
                 )
             );
 
@@ -255,34 +227,36 @@
             // 边缘检测
             s.touchObject.edgeHit = false;
 
-            if ((s.index <= 0 && direction === "right") || (s.index + 1 >= s.slideCount && direction === "left")) {
+            if ((wrap.index <= 0 && (direction === "right" || direction === "up")) 
+                || (wrap.index + 1 >= wrap.slideCount && (direction === "left" || direction === "down"))) {
                 swipeLength = swipeLength * s.edgeFriction;
                 s.touchObject.edgeHit = true;
             }
 
-            s.position = s.lastPosition + swipeLength * (direction === "right" ? 1 : -1);
+            wrap.position = wrap.lastPosition + swipeLength * (direction === "right" || direction === "up" ? 1 : -1);
 
             // 输出调试信息
             if(s.params.debug) {
                 console.log('direction:' + direction
-                    + ',transform:'+ s.wrapper.css('transform')
+                    + ',transform:'+ wrapper.css('transform')
                     + ',s.touchObject.curX:' + parseInt(s.touchObject.curX)
                     + ',s.touchObject.startX:' + parseInt(s.touchObject.startX)
                     + ',swipeLength:' + parseInt(swipeLength)
-                    + ',position:' + s.position);
+                    + ',position:' + wrap.position);
             }
 
-            var xy = isH() ?  Math.ceil(s.position) + 'px, 0px' : '0px, ' +  Math.ceil(s.position) + 'px';
-            s.wrapper.css(s.animType, 'translate(' + xy + ')');
+            s.applyTransform(Math.ceil(wrap.position), 0, false);
+            // var xy = isH() ?  Math.ceil(wrap.position) + 'px, 0px' : '0px, ' +  Math.ceil(wrap.position) + 'px';
+            // wrapper.css(s.animType, 'translate(' + xy + ')');
         };
         s.swipeEnd = function(event) {
             var direction = s.swipeDirection();
             s.shouldClick = (s.touchObject.swipeLength > 10) ? false : true;
             s.autoplayPaused = false;
 
-            if (!s.dragging || s.touchObject.curX === undefined) {
-                return false;
-            }
+            if(s.shouldClick) return false;
+            if (!s.dragging || s.touchObject.curX === undefined) return false;
+
             s.dragging = false;//终止拖拽行为
 
             if (s.touchObject.edgeHit === true) {
@@ -292,7 +266,7 @@
             if (s.touchObject.swipeLength >= s.touchObject.minSwipe) {
                 s.trigger("onSwipe"+ direction[0].toUpperCase() + direction.substring(1), event);
             } else {
-                if (s.touchObject.startX !== s.touchObject.curX) {
+                if (s.touchObject.startX !== s.touchObject.curX || s.touchObject.startY !== s.touchObject.curY) {
                     s.touchObject = {};
                 }
             }
@@ -301,9 +275,9 @@
 
             // 开启惯性滑动
             if(s.params.inertia && s.touchObject.swipeLength >= s.touchObject.minSwipe && (s.touchObject.endTime - s.touchObject.startTime < s.touchDelay)) {
-                
                 // v0 = s / t;
-                var v = (s.touchObject.curX - s.touchObject.startX) / (s.touchObject.endTime - s.touchObject.startTime);
+                var sl = isH() ? (s.touchObject.curX - s.touchObject.startX) : (s.touchObject.curY - s.touchObject.startY);
+                var v = sl / (s.touchObject.endTime - s.touchObject.startTime);
                 var that = this;
 
                 (function() {
@@ -319,22 +293,22 @@
                         }
 
                         nowV -= v * that.inertiaFriction;
-                        var moveX = Math.ceil(nowV * t);
+                        var moveL = Math.ceil(nowV * t);
 
                         // 速度方向变化表示速度达到0了,拉到最左侧和最右侧
                         if (dir * nowV < 0
-                            || that.lastPosition <= that.slideWidth * that.slideCount * -1
-                            || that.lastPosition >= 0) {
-
+                            || wrap.lastPosition <= wrap.slideWH * wrap.slideCount * -1
+                            || wrap.lastPosition > 0) {
+                
                             clearInterval(interval);
 
                             //根据坐标停靠至最近的板块
                             that.slideTo(that.getIndex());
                             return;
                         }
-                        // 实时存储为最终位置
-                        that.lastPosition = that.position = that.position + moveX;
-                        that.applyTransform(that.position, 0, t);
+                        // 实时存储为最终位置                        
+                        wrap.lastPosition = wrap.position = wrap.position + moveL;
+                        that.applyTransform(wrap.position, t);
                     }
                 })();
             } else {
@@ -353,7 +327,7 @@
             s.touchObject.fingerCount = event.originalEvent && event.originalEvent.touches !== undefined ?
                 event.originalEvent.touches.length : 1;
 
-            s.touchObject.minSwipe = s.slideWidth * s.params.touchRatio;
+            s.touchObject.minSwipe = wrap.slideWH * s.params.touchRatio;
 
             switch (event.type) {
                 case 'touchstart':
@@ -373,16 +347,26 @@
         Initialize
         ===========================*/
         s.init = function() {
-            // 设置元素宽度px和个数
-            s.slideWidth = s.slide.outerWidth(true);
-            s.slideCount = s.slide.size();
+
+            container =  container === window ? $(window) : $(container);
+            wrapper = container.find(s.params.wrapperClass);
+            slide = wrapper.find(s.params.slideClass);
+
+            wrap = {
+                position: 0,
+                lastPosition: 0,//定位
+                index: 0,//最后一次的下标
+                slideCount: slide.size(),//个数
+                slideWH: isH() ? slide.outerWidth(true) : slide.outerHeight(true),//元素宽高
+            };
 
             // 添加事件监听器
             if(!s.unresize){
                 $(window).on('resize', this, false);
             }
-            s.container.on('touchstart mousedown touchmove mousemove touchend mouseup touchcancel mouseleave click', function(event){
+            container.on('touchstart mousedown touchmove mousemove touchend mouseup touchcancel mouseleave click', function(event) {
                 s.swipeHandle(event);
+                event.preventDefault();
             });
 
             //设置CSS Transform属性
